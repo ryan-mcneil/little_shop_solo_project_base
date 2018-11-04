0b7219d4783f7df325ee74ca8cb98e6009a48d56
Ryan McNeil <ryan.mcneil288@gmail.com>
2018-11-03 19:59:36 -0600

    updated seed

--- db/migrate/20181103210717_add_address_ref_to_orders.rb
+++
@@ -1,5 +0,0 @@
-class AddAddressIdToOrder < ActiveRecord::Migration[5.1]
-  def change
-    add_reference :orders, :addresses, foreign_key: true
-  end
-end
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,10 +8,19 @@ Item.destroy_all
 User.destroy_all
 
 admin = create(:admin)
-user = create(:user)
+user_1, user_2 = create_list(:user, 2)
 merchant_1 = create(:merchant)
 
 merchant_2, merchant_3, merchant_4 = create_list(:merchant, 3)
+
+admin_address_1 = create(:address, user: admin, default_add: true)
+user_address_1 = create(:address, user: user_1, default_add: true)
+user_address_2 = create(:address, user: user_1, default_add: false)
+user_address_3 = create(:address, user: user_2, default_add: false)
+user_address_4 = create(:address, user: user_2, default_add: true)
+merchant_address_1 = create(:address, user: merchant_1, default_add: true)
+merchant_address_2 = create(:address, user: merchant_2, default_add: true)
+merchant_address_3= create(:address, user: merchant_3, default_add: true)
 
 item_1 = create(:item, user: merchant_1)
 item_2 = create(:item, user: merchant_2)
@@ -19,20 +28,30 @@ item_3 = create(:item, user: merchant_3)
 item_4 = create(:item, user: merchant_4)
 create_list(:item, 10, user: merchant_1)
 
-order = create(:completed_order, user: user)
+order = create(:completed_order, user: user_1, address: user_address_1)
 create(:fulfilled_order_item, order: order, item: item_1, price: 1, quantity: 1)
 create(:fulfilled_order_item, order: order, item: item_2, price: 2, quantity: 1)
 create(:fulfilled_order_item, order: order, item: item_3, price: 3, quantity: 1)
 create(:fulfilled_order_item, order: order, item: item_4, price: 4, quantity: 1)
 
-order = create(:completed_order, user: user)
+order = create(:completed_order, user: user_1, address: user_address_1)
 create(:fulfilled_order_item, order: order, item: item_1, price: 1, quantity: 1)
 create(:fulfilled_order_item, order: order, item: item_2, price: 2, quantity: 1)
 
-order = create(:completed_order, user: user)
+order = create(:completed_order, user: user_1, address: user_address_1)
 create(:fulfilled_order_item, order: order, item: item_2, price: 2, quantity: 1)
 create(:fulfilled_order_item, order: order, item: item_3, price: 3, quantity: 1)
 
-order = create(:completed_order, user: user)
+order = create(:completed_order, user: user_1, address: user_address_1)
 create(:fulfilled_order_item, order: order, item: item_1, price: 1, quantity: 1)
 create(:fulfilled_order_item, order: order, item: item_2, price: 2, quantity: 1)
+
+order = create(:completed_order, user: user_2, address: user_address_2)
+create(:fulfilled_order_item, order: order, item: item_1, price: 1, quantity: 1)
+create(:fulfilled_order_item, order: order, item: item_2, price: 2, quantity: 1)
+create(:fulfilled_order_item, order: order, item: item_4, price: 4, quantity: 1)
+
+order = create(:completed_order, user: user_2, address: user_address_2)
+create(:fulfilled_order_item, order: order, item: item_1, price: 1, quantity: 1)
+create(:fulfilled_order_item, order: order, item: item_2, price: 2, quantity: 1)
+create(:fulfilled_order_item, order: order, item: item_3, price: 3, quantity: 1)
